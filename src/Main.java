3cfb0294c48ae0f62e2991abb0ea502d79ef9ee3
Christian Hayward <hayward.christian@hotmail.com>
2026-03-27 16:32:28 -0400

    First initial commit start to a Physics Engine. Nothing right now just a planner and some inspiration notes. Good Luck to myself!

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    /*
+    This will be the Main class for the Physics Engine, first thing to try is a ball falling.
+    It doesn't matter how long it takes and if you have to search something up you can!
+    This is a huge project but don't stress too much about it!
+    This is for you and you only, it's a learning process and you'll get much better!
+     */
+
+    // TODO: When on Run, have a window pop up
+
+}
