782828510dc6091f704b199348d16b9c7f129628
Christian Hayward <hayward.christian@hotmail.com>
2026-04-03 14:54:38 -0400

    Gravity is up, thought it was going to be much harder but now the real challenge is making walls for the ball to bounce off of!

--- src/Ball.java
+++ src/Ball.java
@@ -1,17 +1,20 @@
-import static time.Time.deltaTime;
-
 public class Ball {
 
     // Ball movement and logic goes in here
+
+    double gravity = 400;
 
     double posX = 100.0;
     double posY = 100.0;
 
     double velX = 40.0;
-    double velY = 20.0;
+    double velY = 0;
 
     public void update(double deltaTime) {
         // Position of the ball and how fast the ball will move in set position
+
+        velY += gravity * deltaTime;
+
         posX += velX * deltaTime;
         posY += velY * deltaTime;
     }
