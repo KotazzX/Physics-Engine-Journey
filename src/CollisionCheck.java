1935e308eb416796017581f2ec7f97bab768a934
Christian Hayward <hayward.christian@hotmail.com>
2026-04-05 15:05:45 -0400

    new class added CollisionCheck.java, hopefully the starting of what I need in the class (WARNING: a couple of warnings but it's a start)

---
+++ src/CollisionCheck.java
@@ -0,0 +1,10 @@
+public class CollisionCheck {
+
+    // if the ball touches the border make the ball or border switch color
+
+    Ball ball = new Ball();
+    Border border = new Border();
+    Renderer render = new Renderer(ball);
+
+
+}
