904873487e245919157d8574da56f564262f0a09
Christian Hayward <hayward.christian@hotmail.com>
2026-04-11 15:17:48 -0400

    Ball now changes colors! when I mean colors, just know it's changing multiple times.

--- src/CollisionCheck.java
+++ src/CollisionCheck.java
@@ -1,3 +1,5 @@
+import java.awt.*;
+
 public class CollisionCheck{
 
     // if the ball touches the border make the ball or border switch color
@@ -6,6 +8,18 @@ public class CollisionCheck{
 
         if (ball.posY > border.posY) {
             System.out.println("Boom, ball has hit the border" + ball.posY);
+            ball.ballColor = newColor();
         }
     }
+
+    private static Color newColor() {
+        // New colors for when ball goes below the border
+
+        int r = (int)(Math.random() * 256);
+        int g = (int)(Math.random() * 256);
+        int b = (int)(Math.random() * 256);
+
+        return new Color(r, g, b);
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -54,7 +54,6 @@ public class Main {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-
         }
     }
 }
