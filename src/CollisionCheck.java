95cf747c66ebdd9e4689cdc4109f1dde5e948943
Christian Hayward <hayward.christian@hotmail.com>
2026-04-13 06:39:00 -0400

    Ball now bounces with the weight of gravity but not the best will fix soon but gravity looks nice to me as a beginner and would love to explore more!!!

--- src/CollisionCheck.java
+++ src/CollisionCheck.java
@@ -2,22 +2,24 @@ import java.awt.*;
 
 public class CollisionCheck{
 
-    // if the ball touches the border make the ball or border switch color
+    // if the border touches the wall, let it move negative
 
     public static void check(Ball ball, Border border) {
 
         if (ball.posY > border.posY) {
             System.out.println("Boom, ball has hit the border" + ball.posY);
             ball.ballColor = newColor();
+
+            ball.gravity -= 400.0;
         }
     }
 
     private static Color newColor() {
         // New colors for when ball goes below the border
 
-        int r = (int)(Math.random() * 256);
-        int g = (int)(Math.random() * 256);
-        int b = (int)(Math.random() * 256);
+        int r = 11;
+        int g = 41;
+        int b = 200;
 
         return new Color(r, g, b);
     }
